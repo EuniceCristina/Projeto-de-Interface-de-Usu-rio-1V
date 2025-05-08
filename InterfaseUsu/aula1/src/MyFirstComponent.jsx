0eb50bab01d7368bdec2815c785906987c39884e
eunicecristina <eunicecristina0510@gmail.com>
2025-05-08 16:50:34 -0300

    commit

---
+++ InterfaseUsu/aula1/src/MyFirstComponent.jsx
@@ -0,0 +1,17 @@
+function MyFirstComponent (){
+
+    const nome = "eunice cristina";
+
+    return(
+        <>
+        <div>
+            <h1>Hello World!</h1>
+        </div>
+        <div>
+            <p>Seu nome é {nome}</p>
+        </div>
+        </>
+    );
+}
+
+export default MyFirstComponent
