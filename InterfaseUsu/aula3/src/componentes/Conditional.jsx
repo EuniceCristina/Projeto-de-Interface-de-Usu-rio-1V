77aebd45a7051206aa78fdb96d2349ff13482785
eunicecristina <eunicecristina0510@gmail.com>
2025-05-19 06:34:33 -0300

    commit

--- InterfaseUsu/aula3/src/componentes/Conditional.jsx
+++ InterfaseUsu/aula3/src/componentes/Conditional.jsx
@@ -1,23 +1,26 @@
 
 import Info1v from "./Info1v";
 import EscopoReduz from "./EscopoReduz";
-import {use, useState} from "react"
+import {useState} from "react"
 
 export default function Conditional(){
     let contente = ''
-    let [opcao, setOpcao] = useState(muda=false);
+    
+    let [opcao, setOpcao] = useState(false);
 
-    if (muda){
+    if (opcao){
         contente = <Info1v/>
+        
     }
     else{
         contente=<EscopoReduz/>
+       
     }
 
     return(
         <>
             {contente}
-            <button onClick={() => setOpcao(!muda)}>Mude</button>
+            <button onClick={() => setOpcao(!opcao)}>Mude</button>
         </>
     )
 }
--- InterfaseUsu/aula3/src/componentes/Info1v.jsx
+++ InterfaseUsu/aula3/src/componentes/Info1v.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react"
 export default function Info1v(){
-     let valor = 0
-     let [valor2, setValor2] = useState(0);
+     let [valor, setValor] = useState(0);
+     let [valor2, setValor2] = useState(1);
      
      function numeric(){
-        let variavel = valor2*2
+        let variavel = valor2*2*2
         setValor2(variavel)
      }
      
@@ -12,8 +12,8 @@ export default function Info1v(){
         <>
         <div>
             <h1>Opa</h1>
-            <button onClick={() => setValor2(valor2+1)}>
-                Cliques {valor2}
+            <button onClick={() => setValor(valor+1)}>
+                Cliques {valor}
             </button>
             <button onClick={numeric}>
                 Cliques {valor2}
