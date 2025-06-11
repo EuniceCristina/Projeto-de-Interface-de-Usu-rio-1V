f3aa15b0066b623dde39fe0a7bedf71bfdd6bd2a
eunicecristina <eunicecristina0510@gmail.com>
2025-06-11 09:48:08 -0300

    teste

--- InterfaseUsu/atividades/dropdown/src/componentes/drop.jsx
+++ InterfaseUsu/atividades/dropdown/src/componentes/drop.jsx
@@ -2,11 +2,12 @@ import './drop.css'
 import {useState} from 'react';
 export default function Drop(){
     let [ver,setVer] = useState(false)
-    let selecionado = ''
+
 
     function mudarMenu(){
        setVer(!ver)
     }
+    
 
     return(
         <>
@@ -14,12 +15,13 @@ export default function Drop(){
         <select name="drop" id="drop" size={3} style={{display : ver ? 'flex' : 'none'}}>
 
            
-            <option value="1" onClick={verSelecionado}>Um</option>
+            <option value="1">Um</option>
             <option value="2">Dois</option>
             <option value="3">Tres</option>
 
            
         </select>
+        
         </>
     )
 }
