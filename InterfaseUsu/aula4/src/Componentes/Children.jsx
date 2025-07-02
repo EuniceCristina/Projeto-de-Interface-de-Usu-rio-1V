63010d321c9d3615f6ca4a6f659f451f4355563b
eunicecristina <eunicecristina0510@gmail.com>
2025-07-02 10:49:20 -0300

    commit

--- InterfaseUsu/aula4/src/Componentes/Children.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function Children({children}){
-
-    return(
-        <div style={{border: "4px solid green", padding: "20px"}}>
-            {children}
-        </div>
-    )
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Projeto-de-Interface-de-Usu-rio-1V
-Dedicado ao estudo da matéria do curso de informática para internet. IFRN Campus Caicó
